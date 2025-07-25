5cafcce56034c1ba00f20a2632e2cfc7fa712ae9
“gaulduer” <gaulduer@gmail.com>
2025-07-25 16:45:09 -0400

    Anoother url change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://gaulduer.github.io/coursera2/",
   plugins: [react()],
+  base: "/coursera2/",
 })
